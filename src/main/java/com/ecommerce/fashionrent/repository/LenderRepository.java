520a3112d9030c39b369da9abb544bb168f6874b
vinithandelwal <vsir0002@student.monash.edu>
2023-12-27 19:08:24 +0530

    minor bug fixed.

--- src/main/java/com/ecommerce/fashionrent/repository/LenderRepository.java
+++ src/main/java/com/ecommerce/fashionrent/repository/LenderRepository.java
@@ -14,7 +14,8 @@ public interface LenderRepository extends JpaRepository<ProductLenders, Integer>
             with productimage as (
                select array_agg(imageurl) AS result_array,productid from productimageurl p2 group by productid)
                select p.id as productId, p.productname as productName, p.rrp, p.rentprice as rentPrice,
-               p.brand, p.occasion as occasion, p.category,p.type as type,p.fit as fit,p.size,p.gender,
+               p.brand, p.occasion as occasion, p.category,p.type as type,p.fit as fit,p.size,p.gender,p.availablefrom as availableFrom,
+               p.securitydeposit as securityDeposit,
                pi.result_array as imageUrl from productlenders p
                join productimage pi ON pi.productid = p.id  
                where p.lenderid = :userId
