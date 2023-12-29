c1857eef94c014d7aa438f55ddc7e7257ab78bc5
vinithandelwal <vsir0002@student.monash.edu>
2023-12-29 17:10:00 +0530

    allow all origans

--- src/main/java/com/ecommerce/fashionrent/config/WebMvcConfig.java
+++ src/main/java/com/ecommerce/fashionrent/config/WebMvcConfig.java
@@ -16,10 +16,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins(allowedOrigins)
+//        .allowedOrigins(allowedOrigins)
         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
         .allowedHeaders("*")
-        .allowCredentials(false)
+        .allowCredentials(true)
         .maxAge(MAX_AGE_SECS);
     }
 }
