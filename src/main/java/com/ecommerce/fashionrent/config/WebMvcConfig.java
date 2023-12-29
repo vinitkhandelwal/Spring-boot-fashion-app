23651b49b9af50a0d8d68bb02fa38ba77039bdb6
vinithandelwal <vsir0002@student.monash.edu>
2023-12-29 17:18:51 +0530

    add allow origin pattern

--- src/main/java/com/ecommerce/fashionrent/config/WebMvcConfig.java
+++ src/main/java/com/ecommerce/fashionrent/config/WebMvcConfig.java
@@ -17,6 +17,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
 //        .allowedOrigins(allowedOrigins)
+                .allowedOriginPatterns("*")
         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true)
