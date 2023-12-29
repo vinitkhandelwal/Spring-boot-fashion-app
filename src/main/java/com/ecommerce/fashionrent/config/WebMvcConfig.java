b3dafcacb2fb5fc780568be3b014d95f6842617f
vinithandelwal <vsir0002@student.monash.edu>
2023-12-29 18:46:17 +0530

    revert the changes

--- src/main/java/com/ecommerce/fashionrent/config/WebMvcConfig.java
+++ src/main/java/com/ecommerce/fashionrent/config/WebMvcConfig.java
@@ -2,37 +2,24 @@ package com.ecommerce.fashionrent.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
 
-@Order(-1)
-@Component
-public class WebMvcConfig implements Filter {
+    private final long MAX_AGE_SECS = 3600;
 
+    @Value("${app.cors.allowedOrigins}")
+    private String[] allowedOrigins;
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException, ServletException, IOException {
-
-        System.out.println( "Request Hit to the filter");
-        HttpServletRequest request = (HttpServletRequest) req;
-        HttpServletResponse response = (HttpServletResponse) res;
-
-
-//        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-        response.setHeader("Access-Control-Allow-Origin", "*"); // * = all domainName
-        response.setHeader("Access-Control-Allow-Credentials", "true"); // allow CrossDomain to use Origin Domain
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
-        response.setHeader("Access-Control-Max-Age", "3600"); // Preflight cache duration in browser
-//        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
-        response.setHeader("Access-Control-Allow-Headers", "*"); // all header
-
-        chain.doFilter(req, res);
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+        .allowedOrigins(allowedOrigins)
+        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+        .allowedHeaders("*")
+        .allowCredentials(true)
+        .maxAge(MAX_AGE_SECS);
     }
 }
